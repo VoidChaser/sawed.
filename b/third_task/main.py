71fc7fc053ab2a29a962641b843a7c66aaeb54c6
Void_chaser <troxa75@yandex.ru>
2023-04-26 23:18:38 +0300

    new index carousel

--- b/third_task/main.py
+++ b/third_task/main.py
@@ -31,8 +31,6 @@ def main():
     create_admin_user('trofi', 'Kukush', main_admin_log, main_admin_pass)
     create_meet_img('static/welcome.jpg')
     meet_post()
-    # add_colounists(db_session)
-    # add_deploying_job(db_session)
     app.run()
 
 
@@ -175,9 +173,6 @@ def create_meet_img(path):
     file = File()
     file.user_id = db_ses.query(User).filter(User.admin_rules == 1).first().id
     file.file_link = path
-    # file.file_mini_link = create_miniature(path)
-    # print(file.file_link, file.file_mini_link)
-    # print(path)
 
     db_ses.add(file)
     db_ses.commit()
@@ -198,72 +193,9 @@ def meet_post():
     post.files_linked = ', '.join([quer.file_link])
     post.files_count = 1
 
-
-    # post.file_link = db_ses.query(File).filter(File.user_id == post.post_creator_id).first()
     print(post.files_linked)
     db_ses.add(post)
     db_ses.commit()
-
-
-
-
-
-
-# def meet_post(db_ses):
-#     file = File()
-#     file.
-
-
-# def add_deploying_job(db_ses):
-#     job = Jobs()
-#     job.team_leader = 1
-#     job.job = 'deployment of residential modules 1 and 2'
-#     job.work_size = 15
-#     job.collaborators = '2, 3'
-#     job.start_date = datetime.datetime.now()
-#     job.is_finished = False
-#
-#     db_sess = db_ses.create_session()
-#     db_sess.add(job)
-#     db_sess.commit()
-#
-#
-# def add_colounists(db_ses):
-#     user = User()
-#     user.surname = 'Scott'
-#     user.name = 'Ridley'
-#     user.age = 21
-#     user.position = 'captain'
-#     user.speciality = 'research engineer'
-#     user.address = 'module_1'
-#     user.email = "scott_chief@mars.org"
-#
-#     user1 = User()
-#     user1.surname = 'Mickey'
-#     user1.name = 'Mouse'
-#     user1.age = 22
-#     user1.position = 'employer'
-#     user1.speciality = 'cleaner'
-#     user1.address = 'module_2'
-#     user1.email = "mickey@mars.org"
-#
-#     user2 = User()
-#     user2.surname = 'Onion'
-#     user2.name = 'Skycryer'
-#     user2.age = 19
-#     user2.position = 'junior spec.'
-#     user2.speciality = 'pilot'
-#     user2.address = 'module_3'
-#     user2.email = "onion_skycryer@mars.org"
-#
-#     db_sess = db_ses.create_session()
-#     db_sess.add(user)
-#     db_sess.add(user1)
-#     db_sess.add(user2)
-#
-#     db_sess.commit()
-
-
 
 
 if __name__ == '__main__':
